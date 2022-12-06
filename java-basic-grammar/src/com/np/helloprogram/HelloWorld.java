41289c75df85ab1daa368f59ef625910ac29f0b6
宁鹏 <1226281140@qq.com>
2022-12-06 23:10:46 +0800

    2022.12.06,np,idea测试第二次git提交代码

--- java-basic-grammar/src/com/np/helloprogram/HelloWorld.java
+++ java-basic-grammar/src/com/np/helloprogram/HelloWorld.java
@@ -1,10 +1,16 @@
 package com.np.helloprogram;
 
 /**
- *
+ * @Author NingPeng
+ * @ClassName:HelloWorld
+ * @Email: ningpeng97@163.com
+ * @Description: 第一个java程序
+ * @Date: 2022/12/6
+ * @Version: v1.0
  */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello!!!World!!!");
+
     }
 }
---
+++ java-basic-grammar/src/com/np/helloprogram/HelloWorld2.java
@@ -0,0 +1,16 @@
+package com.np.helloprogram;
+
+/**
+ * @Author NingPeng
+ * @ClassName:HelloWorld2
+ * @Email: ningpeng97@163.com
+ * @Description: 第一个hello程序
+ * @Date: 2022/12/6
+ * @Version: v1.0
+ */
+public class HelloWorld2 {
+    public static void main(String[] args) {
+        System.out.println("Hello,world!!!");
+        System.out.println("Hello,Git!!!");
+    }
+}

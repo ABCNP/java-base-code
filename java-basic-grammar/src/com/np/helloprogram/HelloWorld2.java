18808d552f8096e7a71084d07047753758975c16
宁鹏 <1226281140@qq.com>
2022-12-06 23:44:16 +0800

    2022.12.06,np,idea测试第三次提交master代码,代码冲突

--- java-basic-grammar/src/com/np/helloprogram/HelloWorld2.java
+++ java-basic-grammar/src/com/np/helloprogram/HelloWorld2.java
@@ -12,5 +12,6 @@ public class HelloWorld2 {
     public static void main(String[] args) {
         System.out.println("Hello,world!!!");
         System.out.println("Hello,Git!!!");
+        System.out.println("测试合并代码");
     }
 }
